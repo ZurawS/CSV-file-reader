88edda94b528cfcb95f3746b3a7c4c6da665316f
PawZet <pawelzurawski898@gmail.com>
2021-09-07 16:53:05 +0200

    commit_3-refactor-with-generics

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,21 +1,11 @@
-import { dateStringToDate } from "./utils";
 import fs from "fs";
-import { MatchResult } from "./MatchResult";
 
-type MatchDataTopple = [
-  Date,
-  string,
-  string,
-  number,
-  number,
-  MatchResult,
-  string
-];
-
-export class CsvFileReader {
-  data: MatchDataTopple[] = [];
+export abstract class CsvFileReader<TypeOfData> {
+  data: TypeOfData[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): TypeOfData;
 
   read(): void {
     this.data = fs
@@ -24,16 +14,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map((row: string[]): MatchDataTopple => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ];
-      });
+      .map(this.mapRow);
   }
 }
